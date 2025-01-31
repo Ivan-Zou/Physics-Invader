1f208d2d1bb5b08607771479236d1c85f495e678
Ivan Zou <ivanzou@seas.upenn.edu>
2025-01-31 15:19:45 -0500

    added ammo pick up sound

--- Assets/Scripts/CollectableAmmo.cs
+++ Assets/Scripts/CollectableAmmo.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CollectableAmmo : MonoBehaviour {
+    public AudioClip ammoPickUpSound;
     void OnCollisionEnter(Collision collision) {
         Collider collider = collision.collider;
 
         if (collider.CompareTag("Player")) {
             PlayerShip player = collider.gameObject.GetComponent<PlayerShip>();
-            // Increase ammo count
+            // If the ammo count is not at capacity
             if (player.ammoCount < player.maxAmmo) {
+                // Increase ammo count
                 player.ammoCount++;
+                // Play ammo pick up sound
+                AudioSource.PlayClipAtPoint(ammoPickUpSound, gameObject.transform.position);
                 // destory the ammo
                 Destroy(gameObject);
             }
