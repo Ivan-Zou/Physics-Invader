e8805ec4bf720fd141d985cc0ea633b4d6bd0d56
Ivan Zou <ivanzou@seas.upenn.edu>
2025-02-01 16:04:53 -0500

    changed drop spawn position

--- Assets/Scripts/SmallAlienInvader.cs
+++ Assets/Scripts/SmallAlienInvader.cs
@@ -28,7 +28,7 @@ public class SmallAlienInvader : MonoBehaviour {
         // Chance to drop either an ammoDrop or lifeDrop, but not both
         if (Random.value < dropChance) {
             GameObject dropItem = (Random.value < 0.5f) ? ammoDrop : lifeDrop;
-            Vector3 spawnPos = gameObject.transform.position + new Vector3(0, 0, -0.5f);
+            Vector3 spawnPos = gameObject.transform.position + new Vector3(0, 0, -0.3f);
             Instantiate(dropItem, spawnPos, Quaternion.identity);
         }
         // Destroy object
